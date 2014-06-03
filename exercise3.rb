3703ea9520b8418f2079208d3210c2a0dff9f1c7
Richard Martin <richardgeorgemartin@gmail.com>
2014-06-03 14:29:40 -0400

    commit name and birth date code for exercise3

--- exercise3.rb
+++ exercise3.rb
@@ -1,3 +1,12 @@
+# ask the visitor his or her name
+
 puts "What is your name?"
 name = gets.chomp
 puts "Hi #{name}!"
+
+# ask the visitor how old he is and compute date of birth
+
+puts "How old are you, dude?"
+age = gets.chomp
+birth_date = 2014 - age.to_i
+puts "#{name}, you were born in #{birth_date}"
